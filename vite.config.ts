0e546293a40fefd78fe046720518fe36068a723a
爱华 <1348844909@qq.com>
2022-09-14 19:34:28 +0800

    feat/增加scss

--- vite.config.ts
+++ vite.config.ts
@@ -1,7 +1,44 @@
-import { defineConfig } from 'vite'
-import vue from '@vitejs/plugin-vue'
-
+import { defineConfig } from 'vite';
+import vue from '@vitejs/plugin-vue';
+import path from 'path';
+// 配置element-plus
+// import Components from 'unplugin-vue-components/vite'
+// import { ElementPlusResolver } from 'unplugin-vue-components/resolvers'
 // https://vitejs.dev/config/
+console.log(path.resolve(__dirname, './src'));
 export default defineConfig({
-  plugins: [vue()]
-})
+  plugins: [
+    vue(),
+    // Components({
+    // resolvers: [ElementPlusResolver()],
+    // })
+  ],
+  resolve: {
+    alias: {
+      '@': path.resolve(__dirname, 'src'),
+      '@components': path.resolve(__dirname, 'src/components'),
+      '@styles': path.resolve(__dirname, 'src/styles'),
+      '@views': path.resolve(__dirname, 'src/views'),
+      '@utils': path.resolve(__dirname, 'src/utils'),
+      '@hooks': path.resolve(__dirname, 'src/hooks'),
+      '@commits': path.resolve(__dirname, 'src/store/const'),
+    },
+  },
+  server: {
+    open: '/test/canvas', // 浏览器自动打开
+  },
+  css: {
+    preprocessorOptions: {
+      // less: {
+      //   javascriptEnabled: true,
+      //   additionalData: `@import "${path.resolve(
+      //     __dirname,
+      //     'src/assets/styles/base.less'
+      //   )}";`,
+      // },
+      scss: {
+        // additionalData: '@import "src/assets/styles/var.scss";'
+      },
+    },
+  },
+});
